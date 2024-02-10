f30772d6cbb7a68f72cb3b7f070579fe97105656
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2024-02-10 11:44:41 +0000

    feat: removing ownership set to directory
    #4

--- release-it-entrypoint.sh
+++ release-it-entrypoint.sh
@@ -12,9 +12,6 @@ RELEASE_IT_PLUGINS=${RELEASE_IT_PLUGINS:-}
 SSH_PASSPHRASE="${SSH_PASSPHRASE:-}"
 SSH_PRIVATE_KEY_FILE="${SSH_PRIVATE_KEY_FILE:-}"
 
-# Giving the current user ownership of the directory
-chown -R "$(id -u):$(id -g)" "${PWD}" || true
-
 if [[ -n "${GIT_DIRECTORY}" ]]; then
     # Add safe directory to git global configuration
     git config --global --add safe.directory "${GIT_DIRECTORY}"
